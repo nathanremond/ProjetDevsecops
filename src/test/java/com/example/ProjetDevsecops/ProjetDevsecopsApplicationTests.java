a80df820ac2bd5850771fabf16aff5f76e8e607a
nathanremond <perso.nathan.remond@gmail.com>
2025-04-12 10:25:34 +0200

    Initial commit

---
+++ src/test/java/com/example/ProjetDevsecops/ProjetDevsecopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjetDevsecops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDevsecopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
